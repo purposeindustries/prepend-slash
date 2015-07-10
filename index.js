e7f554709123264c3e0ef56aa1c2d5403b1d29ea
ruff <ftamas.mail@gmail.com>
2015-07-10 12:19:11 +0200

    strict is true by default

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 /**
  * Put a slash at the beginning of the given string.
  *
- * @param  {String} str    a string to be prepended
- * @param  {Boolean} [strict=false]    if it's true and the first parameter isn't a string, it throws a TypeError
- * @return {String}        the string with leading slash
+ * @param {String} str - a string to be prepended
+ * @param {Boolean} [strict=true] - if it's true and the first parameter isn't a string, it throws a TypeError
+ * @return {String} - the string with leading slash
  */
 module.exports = function (str, strict) {
-  strict = !!strict;
+  strict = (typeof strict === 'undefined') ? true : !!strict;
   if (strict && typeof str !== 'string') {
     throw new TypeError('Expected a string as the first parameter');
   }
